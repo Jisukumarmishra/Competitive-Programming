1760577418e040bfc958ebd435466e7e9fb2b06e
mishra <jisumishra4@gmail.com>
2026-03-20 17:58:40 +0530

    Solve MoveBracket Question At code Force using Greedy Approach

--- Code_Forces/moveBrackets.java
+++ Code_Forces/moveBrackets.java
@@ -5,5 +5,42 @@ public class moveBrackets {
     Scanner sc = new Scanner(System.in);
     int  t = sc.nextInt();
     
+    while (t-- > 0) { //t-- → use value of t, then decrease it
+      int  n = sc.nextInt();
+      String s = sc.next();
+
+      int fineBracket = 0;
+      int count = 0;
+
+      for (char c : s.toCharArray()) {
+        if ( c== '(' ) {
+          fineBracket++;
+        } else {
+          fineBracket--;
+        }
+     
+        // below if code is gredy
+        if (fineBracket <0) {// mean no iof wrong bracket comes shopw the answwer = to total number of wring bracket
+        count++;
+        fineBracket =0;
+
+        }
+      }
+   
+      System.out.println(count);
+    }
+
+    
+    
   }
 }
+
+// s = "())()()("
+// (  → balance = 1 // fine = balance
+// )  → balance = 0
+// )  → balance = -1 ❌ → moves/count = 1, reset balance/fine = 0
+// (  → balance = 1
+// )  → balance = 0
+// (  → balance = 1
+// )  → balance = 0
+// (  → balance = 1
